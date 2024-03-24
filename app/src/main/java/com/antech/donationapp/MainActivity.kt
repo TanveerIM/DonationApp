a37ca481510e212f22eacadb56e53c08462099f7
tanveer <tanveeratworks@gmail.com>
2024-03-24 22:44:12 +0530

    setting up the drawerLayout and screen navigation. Add the scrollView in SignUp screen.

--- app/src/main/java/com/antech/donationapp/MainActivity.kt
+++ app/src/main/java/com/antech/donationapp/MainActivity.kt
@@ -2,13 +2,20 @@ package com.antech.donationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.antech.donationapp.databinding.ActivityMainBinding
@@ -32,13 +39,15 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
 
         appBarConfiguratoin = AppBarConfiguration(
-            navController.graph, drawerLayout
-        )
+           setOf(R.id.homeFragment, R.id.aboutAppFragment)/*navController.graph*/, drawerLayout
 
+        )
         setupActionBarWithNavController(navController, appBarConfiguratoin)
-        navView.setupWithNavController(navController)
+        //binding.appBarMain.mainContent.toolbar.setupWithNavController(navController, appBarConfiguratoin)
+        binding.appBarMain.mainContent.toolbar.setNavigationIcon(R.drawable.ic_drawer)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id == R.id.splashScreenFragment) {
@@ -47,12 +56,17 @@ class MainActivity : AppCompatActivity() {
             if (destination.id == R.id.viewPagerFragment) {
                 supportActionBar?.hide()
             }
+            if (destination.id == R.id.homeFragment) {
+                supportActionBar?.show()
+
+            }
+            if (destination.id == R.id.aboutAppFragment) {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                supportActionBar?.setDisplayShowHomeEnabled(true)
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        return true
-    }
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.navHostFragment)
         return navController.navigateUp(appBarConfiguratoin) || super.onSupportNavigateUp()
